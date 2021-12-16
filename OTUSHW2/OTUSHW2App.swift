969458bf8452ffa0f5884e761738534b0d59449d
DIM <dimitrol7171@gmail.com>
2021-12-16 08:29:17 +0300

    Initial Commit

---
+++ OTUSHW2/OTUSHW2App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSHW2App.swift
+//  OTUSHW2
+//
+//  Created by Дмитрий Мальцев on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUSHW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
